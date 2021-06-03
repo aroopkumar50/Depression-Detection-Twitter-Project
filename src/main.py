16b862ef67599b548b721542830badfd02368892
AROOP KUMAR <aroopkumarofficial@gmail.com>
2021-06-04 01:49:54 +0530

    Dropdown added in index.html

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
